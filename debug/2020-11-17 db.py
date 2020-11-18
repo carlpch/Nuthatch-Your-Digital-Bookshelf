733518ba1e1e63e3cf943784741ad9ecf6fc2571
Carl Huang <carlls007@gmail.com>
2020-11-17 22:28:20 -0500

    improved sync function

---
+++ debug/2020-11-17 db.py
@@ -0,0 +1,21 @@
+from app import db
+from models import User, Book
+
+bobofish = User.query.get(1)
+b = bobofish.user_books()
+
+
+# this line replicates the wrong shelf
+[(i.timestamp, i.title) for i in b.order_by(Book.timestamp.asc()).all()][:5]
+
+# this line replicates the wrong shelf
+[(i.timestamp, i.title)  for i in b.order_by(Book.timestamp.desc()).all()][-5:]
+
+[(i.timestamp, i.title)  for i in b.order_by(Book.timestamp.desc()).all()][:5]
+
+
+
+
+Book.query.filter(Book.user_id == self.username) 
+
+Book.query.filter(Book.user_id == 'bobofish', Book.zotero_key == 'ddd')
